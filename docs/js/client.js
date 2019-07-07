d6fab091e8b391ddc9c597e439387e10fe57683b
Radosław Partyka <r.partyka30@gmail.com>
2019-07-07 17:15:55 +0200

    Bugfix

--- docs/js/client.js
+++ docs/js/client.js
@@ -1,15 +1,17 @@
-setTimeout(() => { let onlineReq = new XMLHttpRequest(), onlineData;
+setTimeout(() => {
+  let onlineReq = new XMLHttpRequest(), onlineData;
 
-onlineReq.open('GET', 'https://mcapi.us/server/status?ip=krd_online.csrv.pl');
-onlineReq.send();
+  onlineReq.open('GET', 'https://mcapi.us/server/status?ip=krd_online.csrv.pl');
+  onlineReq.send();
 
-onlineReq.onload = () => {
-  onlineData = JSON.parse(onlineReq.responseText)
+  onlineReq.onload = () => {
+    onlineData = JSON.parse(onlineReq.responseText)
 
-  if(onlineData.online) {
-    console.log('Serwer działa!')
+    if(onlineData.online) {
+      console.log('Serwer działa!')
+    }
+    document.write(onlineData.motd);
+    document.write(onlineData.players.now + '/' + onlineData.players.max);
   }
-  document.write(onlineData.motd);
-  document.write(onlineData.players.now + '/' + onlineData.players.max)
 
 }, 1000)
